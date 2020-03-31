c57a380cd7fb6df956ef73958e27feb60d62a191
kdo0129 <eha0129@naver.com>
2020-03-31 14:30:41 +0900

    counter 구현완료

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import CounterContainer from './containers/CounterContainer';
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<CounterContainer />
+		</div>
+	);
 }
 
 export default App;
